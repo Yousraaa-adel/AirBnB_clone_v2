fd8ec4ae7c8d5d364e404cf60362256c3b513095
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-05-28 17:45:31 +0300

    Checking Task 10

--- models/amenity.py
+++ models/amenity.py
@@ -17,8 +17,9 @@ class Amenity(BaseModel, Base):
 
         name = Column(String(128), nullable=False)
         place_amenities = relationship(
-            "Place", secondary="place_amenity", back_populates="amenities"
-            )
+            "Place",
+            secondary="place_amenity", back_populates="amenities")
+
 
     else:
         name = ""
