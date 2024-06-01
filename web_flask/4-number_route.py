492a0541b63d33f379bdbc661fec9ff94d0878fa
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-06-01 21:53:09 +0300

    Flask Task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -14,28 +14,37 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def index():
+    """ Displays a text """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """ Displays a text """
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def cisfun(text):
+    """ Displays a text """
     return "C " + text.replace('_', ' ')
 
 
 @app.route("/python/", defaults={"text": "is_cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def pythoniscool(text):
+    """ Displays a text """
     return "Python " + text.replace("_", " ")
 
 
 @app.route("/number/<n>", strict_slashes=False)
 def isnum(n):
-    return "%d is a number" % n
+    """ Displays a number """
+    try:
+        if n.isdigit():
+            return f"{n} is a number"
+    except ValueError:
+        return "404 Not Found", 404
 
 
 if __name__ == "__main__":
