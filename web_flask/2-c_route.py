44aad798d86c45ee6b63a35eb827ad6c8a958601
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-06-01 21:22:45 +0300

    Flask Task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+    This is a script that starts a Flask web application
+    listening on 0.0.0.0, port 5000
+    Routes:
+        /: display “Hello HBNB!”
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello():
+    return "HBNB"
+
+
+app.route("/c/<text>", strict_slashes=False)
+def cisfun(text):
+    return "C " + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
